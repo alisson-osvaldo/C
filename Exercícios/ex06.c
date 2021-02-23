9b431c5704f73fdb959285bbde215f6be5ca64e6
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-02-23 20:29:55 -0300

    leia 3 notas de um aluno e um quarto número que representa o modelo de média a ser calculada.

--- Exercícios/ex06.c
+++ Exercícios/ex06.c
@@ -1,8 +1,8 @@
 /*
-6. Faça um algoritmo que leia 3 notas de um aluno e um quarto número que representa o modelo de média a
-ser calculada. Caso o modelo seja igual a 1, calcular a média aritmética; caso seja igual a 2, calcular a
-média ponderada com pesos iguais a 5, 3 e 2; caso seja igual a 3, calcular a média harmônica. Repetir este
-procedimento até que uma das notas informadas seja menor que 0.
+6. FaÃ§a um algoritmo que leia 3 notas de um aluno e um quarto nÃºmero que representa o modelo de mÃ©dia a
+ser calculada. Caso o modelo seja igual a 1, calcular a mÃ©dia aritmÃ©tica; caso seja igual a 2, calcular a
+mÃ©dia ponderada com pesos iguais a 5, 3 e 2; caso seja igual a 3, calcular a mÃ©dia harmÃ´nica. Repetir este
+procedimento atÃ© que uma das notas informadas seja menor que 0.
 
 
 Pond = nota1*peso1 + nota2*peso2 + nota3*peso3      Harm =        3
@@ -26,19 +26,18 @@ struct boletin_aluno{
 //armazenando a struct em um vetor para armazenar mais de um aluno
 struct boletin_aluno cad_aluno[TAM];
 
-
 //menu
 int menu(){
 	int op;
 		
 	do{
 		system("cls");
-		printf("## Calcular média ##\n");
-		printf("1.Média aritmética\n");
-		printf("2.Média ponderada\n");
-		printf("3.Média harmonica\n");
+		printf("## Calcular mï¿½dia ##\n");
+		printf("1.Mï¿½dia aritmï¿½tica\n");
+		printf("2.Mï¿½dia ponderada\n");
+		printf("3.Mï¿½dia harmonica\n");
 		printf("4.Sair\n");
-		printf("Opção:/n");
+		printf("Opï¿½ï¿½o:/n");
 		scanf("%i", &op);	
 	}while(op < 1 || op > 4);
 	return op;
@@ -47,17 +46,17 @@ int menu(){
 //procedimento para ler notas
 void notas(){	 
 	int i;
-		
-		for(i = 0; i < TAM; i++){
-			printf("Informe o nome do aluno:\n");
-			scanf("%s", &cad_aluno[i].nome);
-			printf("Informe nota %i:\n", i+1);
-			scanf("%f", &cad_aluno[i].n1);
-			printf("Informe nota %i:\n", i+2);
-			scanf("%f", &cad_aluno[i].n2);
-			printf("Informe nota %i:\n", i+3);
-			scanf("%f", &cad_aluno[i].n3);	
-		}			
+
+	for(i = 0; i < TAM; i++){
+		printf("Informe o nome do aluno:\n");
+		scanf("%s", &cad_aluno[i].nome);
+		printf("Informe nota %i:\n", i+1);
+		scanf("%f", &cad_aluno[i].n1);
+		printf("Informe nota %i:\n", i+2);
+		scanf("%f", &cad_aluno[i].n2);
+		printf("Informe nota %i:\n", i+3);
+		scanf("%f", &cad_aluno[i].n3);	
+	}			
 }
 
 //Procedimento para mostrar Aluno
@@ -72,7 +71,7 @@ void mostraAluno(){
 	}
 }
 
-//função para calcular a média aritmética
+//funÃ§Ã¢o para calcular a mÃ©dia aritmÃ©tica
 float aritmetica(){
 	int i;
 	float media = 0;
@@ -83,25 +82,25 @@ float aritmetica(){
 	return media;
 }
 
-//função para calcular a média ponderada
+//funÃ§Ã¢o para calcular a mÃ©dia ponderada
 float ponderada(int x){
 	int i;
 	float ponderada[TAM];
 	int peso1 = 5;
 	int peso2 = 3;
 	int peso3 = 2;
-	//for para calcular a média de cada aluno
+	//for para calcular a mÃ©dia de cada aluno
 	for(i = 0; i < TAM; i++){
 		ponderada[i]= (cad_aluno[i].n1*peso1 + cad_aluno[i].n2*peso2 + cad_aluno[i].n3*peso3) / (peso1 + peso2 + peso3);	
 	}
 	
-	//for para retornar a média de cada aluno
+	//for para retornar a mÃ©dia de cada aluno
 	for(i = 0; i < TAM; i++){
-		return ponderada[x];// retornando a posição do indice da main(*para não repetir mesmo valor, já q não foi possivel retornar um vetor para main)
+		return ponderada[x];// retornando a posiÃ§Ã£o do indice da main(*para nÃ£o repetir mesmo valor, jÃ¡ q nÃ£o foi possivel retornar um vetor para main)
 	}	
 }
 
-//função para calcular a média harmonica
+//funÃ§Ã£o para calcular a mÃ©dia harmonica
 float harmonica(int x){
 	int i;
 	float harmonica[TAM];
@@ -115,7 +114,7 @@ float harmonica(int x){
 	}
 }
 
-//função para perguntar se deseja calcular novamente?
+//funÃ§Ã¢o para perguntar se deseja calcular novamente?
 char leOpcao(){
 	char op;
 	do{
@@ -143,15 +142,15 @@ void main(){
 				case 1:
 					 aux = aritmetica();
 					 printf("\n");
-					 printf("Média Aritmética: %.1f\n", aux);
+					 printf("Mï¿½dia Aritmï¿½tica: %.1f\n", aux);
 				getch();
 				break;
 							
 				case 2: 
 					for(i = 0; i < TAM; i++){
-						x = ponderada(i); //enviando a posição do indice para a função(*para retornar a média de cada aluno)
+						x = ponderada(i); //enviando a posiÃ§Ã£o do indice para a funÃ§Ã¢o(*para retornar a mÃ©dia de cada aluno)
 						printf("\n");
-						printf("Média Ponderada: %.1f\n", x);
+						printf("MÃ©dia Ponderada: %.1f\n", x);
 					}
 				getch();
 				break;
@@ -160,7 +159,7 @@ void main(){
 					for(i = 0; i < TAM; i++){
 						x = harmonica(i);
 						printf("\n");
-						printf("Média Harmonica: %.1f\n", x);
+						printf("MÃ©dia Harmonica: %.1f\n", x);
 					}
 				getch();			
 				break;								

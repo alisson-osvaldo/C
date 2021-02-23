8bf70c368b21ae87f452db9fd0b1752be51705a2
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-02-23 20:35:20 -0300

    Faça um algoritmo para um programa de apostas da LOTO

--- Exercícios/ex07.c
+++ Exercícios/ex07.c
@@ -1,9 +1,9 @@
 /*
-7. Faça um algoritmo para um programa de apostas da LOTO. O algoritmo deverá ler, inicialmente, as cinco
+7.FaÃ§a um algoritmo para um programa de apostas da LOTO. O algoritmo deverÃ¡ ler, inicialmente, as cinco
 dezenas sorteadas e, a seguir, as apostas dos jogadores contendo:
 
-a. Número da aposta
-b. Quantidade de dezenas apostadas (no máximo 10)
+a. NÃºmero da aposta
+b. Quantidade de dezenas apostadas (no mÃ¡ximo 10)
 c. As dezenas apostadas.
 */
 
@@ -14,11 +14,10 @@ c. As dezenas apostadas.
 
 struct aposta
 {
-	int	numero_da_aposta;	  //Números da aposta do jogador
-	int quantidade_apostadas; //Quantidades de dezenas apostadas(máx 10)
+	int	numero_da_aposta;	  //NÃºmeros da aposta do jogador
+	int quantidade_apostadas; //Quantidades de dezenas apostadas(mÃ¡x 10)
 };
 struct aposta vetJogador[5]; //vetor para armazenar dados do jogo de cada jogador
-
 
 struct dezenas_apostadas
 {
@@ -26,12 +25,12 @@ struct dezenas_apostadas
 };
 struct dezenas_apostadas vetDezenas[10];
 	
-//função para sortear 5 números aleaórios entre 0 e 99
+//funÃ§Ã£o para sortear 5 nÃºmeros aleaÃ³rios entre 0 e 99
 int sorteio(int x){
 	int i, numero[5];
 	
 	for(i = 0; i < 5; i++){
-		numero[i] = 10 + (rand() % 90); //Gerar valor aleatório( min 10, máx 99)
+		numero[i] = 10 + (rand() % 90); //Gerar valor aleatÃ³rio( min 10, mÃ¡x 99)
 	}
 	
 	for(i = 0; i < 5; i++){
@@ -39,33 +38,33 @@ int sorteio(int x){
 	}
 }
 
-//função perguntar quantas dezenas deseja jogar?
+//funÃ§Ã£o perguntar quantas dezenas deseja jogar?
 int quantidade(){
 	int x;
-		
-		printf("\n");
-		//Quantidades de dezenas apostadas(máx 10)
-		do{
-			printf("Quantas dezenas deseja jogar (Máximo 10)?\n");
-			scanf("%i", &x);
-		}while(x > 10);
-		return x;
+	
+	printf("\n");
+	//Quantidades de dezenas apostadas(mÃ¡x 10)
+	do{
+		printf("Quantas dezenas deseja jogar (MÃ¡ximo 10)?\n");
+		scanf("%i", &x);
+	}while(x > 10);
+	return x;
 }
 	
-//função para escolher as dezenas
+//funÃ§Ã£o para escolher as dezenas
 int escolherDezenas(){
 	int i;
 	int dezenas;
 	
-		//escolha as dezenas 
-		scanf("%i", &dezenas);	
-		return dezenas;		
+	//escolha as dezenas 
+	scanf("%i", &dezenas);	
+	return dezenas;		
 }
 	
 
 void main(){
 	setlocale(LC_ALL, "");
-	srand(time(NULL));         //ajustar a semente do randomizador
+	srand(time(NULL)); //ajustar a semente do randomizador
 	int i, x, z;
 	int aux = 0;
 	int numerosSorteados[5];
@@ -73,33 +72,33 @@ void main(){
 	for(i = 0; i < 5; i++){
 		aux = i;
 		
-		//Números da aposta do jogador
-			printf("jogo N-%i", i+1);
-			vetJogador[i].numero_da_aposta = i;
+		//NÃºmeros da aposta do jogador
+		printf("jogo N-%i", i+1);
+		vetJogador[i].numero_da_aposta = i;
 		
-		//Chamar função quantidade()
-			x = quantidade();
-			vetJogador[i].quantidade_apostadas = x;
+		//Chamar funÃ§Ã£o quantidade()
+		x = quantidade();
+		vetJogador[i].quantidade_apostadas = x;
 		
-		//Chamar função escolherDezenas()
-			printf("Escolha %i dezenas:\n", vetJogador[i].quantidade_apostadas);
-			for(i = 0; i < x; i++){
-				z = escolherDezenas();
-				vetDezenas[i].dezenas = z;
-			}
-			printf("Dezenas escolhidas:\n");
-			for(i = 0; i < x; i++){
-				printf("(%i)\t", vetDezenas[i].dezenas);
-			}
+		//Chamar funÃ§Ã£o escolherDezenas()
+		printf("Escolha %i dezenas:\n", vetJogador[i].quantidade_apostadas);
+		for(i = 0; i < x; i++){
+			z = escolherDezenas();
+			vetDezenas[i].dezenas = z;
+		}
+		printf("Dezenas escolhidas:\n");
+		for(i = 0; i < x; i++){
+			printf("(%i)\t", vetDezenas[i].dezenas);
+		}
 			
-			printf("\n\n\n");
+		printf("\n\n\n");
 		
 		i = aux;// acertar indice do primeiro for()
 	}
 	
-	//Números sorteados
+	//NÃºmeros sorteados
 	printf("\n");
-	printf("---Números sorteados---\n");
+	printf("---NÃºmeros sorteados---\n");
 	for(i = 0; i < 5; i++){
 		x = sorteio(i);
 		numerosSorteados[i] = x;
@@ -108,7 +107,6 @@ void main(){
 	}
 			
 }
-
 
 
 

0b3ed8dd1ea25713fee109f168b737ffa9d94085
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-02-23 21:05:52 -0300

    Leia um inteiro do teclado e o retorne somente se este valor for positivo.

---
+++ ex.13.c
@@ -0,0 +1,31 @@
+/*
+13. Elabore uma função que apresente uma mensagem na tela, leia um inteiro do teclado e o retorne somente
+se este valor for positivo.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//função para retornar um valor positivo
+int returnPositivo(){
+	int positivo;
+	
+	printf("Informe um valor positivo:\n");
+	
+	do{
+		scanf("%i", &positivo);
+		if(positivo < 0){
+			printf("O valor não é positivo!!!");
+		}
+	}while(positivo < 0);
+	return positivo;
+}
+
+void main(){
+	setlocale(LC_ALL, "");
+	int p;
+	
+	p = returnPositivo();
+	printf("Valor informado: %i", p);	
+}

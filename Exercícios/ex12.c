887251b50feb5e2a31f110febd573b878c18ac0e
Alisson Osvaldo <alisson.artigas@gmail.com>
2021-02-23 21:32:39 -0300

    Apresente um menu qualquer na tela. Faça a chamada desta função no algoritmo principal.

---
+++ Exercícios/ex12.c
@@ -0,0 +1,46 @@
+/*
+12. Elabore uma função que apresente um menu qualquer na tela. Faça a chamada desta função no algoritmo
+principal.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//Função menu
+int menu(){
+	int genero;
+	
+	do{
+		printf("Escolha um genero musical:\n");
+		printf("1.Rock\n");
+		printf("2.Sertanejo\n");
+		printf("3.Pagode\n");
+		scanf("%i", &genero);
+	}while(genero < 1 || genero > 3);
+	return genero;
+	
+}
+
+void main(){
+	setlocale(LC_ALL, "");
+	int op;
+	
+	op = menu();
+	switch(op){
+		case 1: 
+			printf("\n");
+			printf("você escolheu Rock\n");
+		break;
+		
+		case 2:
+			printf("\n");
+			printf("você escolheu Sertanejo\n");
+		break;
+		
+		case 3:
+			printf("\n");
+			printf("você escolheu Pagode\n");
+		break;
+	}
+}
